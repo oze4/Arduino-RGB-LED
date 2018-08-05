00f9315a5e8525270fd9c720c9be28e079e37283
oze4 <oze41998@yahoo.com>
2018-08-05 03:15:00 -0500

    fixed formatting & added comments

--- rgbled.ino
+++ rgbled.ino
@@ -1,7 +1,8 @@
 /*
  Name:		rgbled.ino
- Created:	8/5/2018 2:28:23 AM
  Author:	Matt
+ 
+ Sketch to control RGB LED
 */
 
 #include "arduino.h"
@@ -9,16 +10,28 @@
 
 // build first led bulb object
 rgbled led1(9, 10, 11); // 9 is the pin Red is plugged into
-						// 10 is the pin Green is plugged into
-						// 11 is the pin Blue is plugged into
+			// 10 is the pin Green is plugged into
+			// 11 is the pin Blue is plugged into
+
+/* ~ if you had a second RGB LED
+rgbled led2(5, 6, 7) // or whichever ports on the Arduino they're plugged into
+		     // 5 is the pin Green is plugged into
+		     // 6 is the pin Blue is plugged into
+		     // 7 is the pin Blue is plugged into
+*/
 
 
+// "run once" - each time the Arduino boots, this runs one time
 void setup() {
 	led1.green();
+	/* if you had a second LED
+	led2.green(); */
 	delay(10);
 }
 
-// the loop function runs over and over again until power down or reset
+// this is what runs over and over and over until the Arduino is powered off
 void loop() {
 	led1.showRgbColorSpectrum(10);
+	/* if you had a second LED
+	led2.showRgbColorSpectrum(10); */
 }
